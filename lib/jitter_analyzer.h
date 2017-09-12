f35f0bbda9ff9dd2318a8724f0a05966095b34b0
Pierre-Anne Bausson <pbausson@lxplus063.cern.ch>
2017-09-12 16:50:26 +0200

    Range plotting try .. aborted

--- lib/jitter_analyzer.h
+++ lib/jitter_analyzer.h
@@ -15,8 +15,8 @@ class jitter_handler{
   jitter_handler(double refFreq){_refFreq = refFreq; };
   double rms_jitter(source_jitter & sj);
   void set_ref_freq(double refFreq){_refFreq = refFreq;};
-  void set_min_freq(double minF){_minFreq = minF;}
-  void set_max_freq(double maxF){_maxFreq = maxF;}
+  void set_min_freq(double minF){_minFreq = minF;};
+  void set_max_freq(double maxF){_maxFreq = maxF;};
  private:
   double integrate( const source_jitter & sj);
   double _maxFreq = 1.1e7;
@@ -29,6 +29,7 @@ class jitter_handler{
 class plot_jitter{
  public:
   void add_plot(const source_jitter & sj);
+  void add_plot_range(const source_jitter & sj, double, double);
   void print_plot();
  private:
   std::vector<TGraph*> _graphs;
