32219814effb454909552c972151028c8c2b3819
Özgür Sahin <sahin@sahinsmac.dyndns.cern.ch>
2018-01-31 14:38:30 +0100

    Now a overlayed waveform graph is automatically generated

--- generate_clock.cc
+++ generate_clock.cc
@@ -2,7 +2,7 @@
 int main (){
   /* generate clock with jitter */
   /* arguments: (nCycles, clock period in ps, RJ in ps, DJ in ps) */
-  generateClock jitterClock(100000,24951,7,38);
+  generateClock jitterClock(100000,24951,8.2,18.6);
   jitterClock.getClockSim();
   return 0;
 }
